e684980aa961e7f8d5d08115d5a94bdf5a6801ce
nilayhangarge <nilayhangarge@gmail.com>
2022-11-13 21:33:17 +0530

    Day-16 Half Done

---
+++ Day-16/main.py
@@ -0,0 +1,17 @@
+from turtle import Turtle, Screen
+
+# import another_module
+# print(another_module.another_variable)
+
+# object = class()
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+
+# Screen is the object & canvaheight is the attribute
+my_screen = Screen()
+
+print(my_screen.canvheight)
+timmy.color("black", "red")  # object.color(pencolor, fillcolor)     #pencolor is border
+timmy.forward(100)
+my_screen.exitonclick()  # object.method()
