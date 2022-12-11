c07a20ad36b5d358f7aca8fa2b42f9f0a484e7e2
Nilay Hangarge <nilayhangarge@gmail.com>
2022-12-12 02:05:15 +0530

    Day-17 Quiz Game Part-4,5
    Signed-off-by: Nilay Nagnath Hangarge <nilayhangarge@gmail.com>

--- Project/Day-17 Quiz Game/main.py
+++ Project/Day-17 Quiz Game/main.py
@@ -5,8 +5,8 @@ from quiz_brain import QuizBrain
 question_bank = []
 for key in question_data:
     # Create objects for text, answer
-    question_text = key["text"]
-    question_answer = key["answer"]
+    question_text = key["question"]
+    question_answer = key["correct_answer"]
 
     # Pass these objects to Question class
     new_question = Question(question_text, question_answer)
@@ -15,4 +15,9 @@ for key in question_data:
     question_bank.append(new_question)
 
 quiz = QuizBrain(question_bank)
-quiz.next_question()
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
--- Project/Day-17 Quiz Game/quiz_brain.py
+++ Project/Day-17 Quiz Game/quiz_brain.py
@@ -2,6 +2,11 @@ class QuizBrain:
     def __init__(self, ques_list):
         self.question_number = 0
         self.question_list = ques_list
+        self.score = 0
+
+    # checking if we're the end of the quiz
+    def still_has_questions(self):
+        return len(self.question_list) > self.question_number
 
     def next_question(self):
         # 'list' is not callable = check the name and parenthesis
@@ -9,8 +14,15 @@ class QuizBrain:
         self.question_number += 1
 
         # asking the questions
-        user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False):")
+        user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False): ")
+        self.check_answer(user_answer, current_question.answer)
 
-
-# checking if the answer was correct
-# checking if we're the end of the quiz
+    # checking if the answer was correct
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            self.score += 1
+            print("You got it right!")
+        else:
+            print("That's wrong.")
+            print(f"The correct answer was: {correct_answer}")
+        print(f"Your current score is: {self.score}/{self.question_number}\n")
