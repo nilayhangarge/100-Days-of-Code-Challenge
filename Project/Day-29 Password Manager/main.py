5b639e6d352ac68139b3ce63300dea974ec49852
Nilay Nagnath Hangarge <nilayhangarge@gmail.com>
2023-09-14 03:18:54 +0530

    Day-30 Improved version of Password Manager
    Signed-off-by: Nilay Nagnath Hangarge <nilayhangarge@gmail.com>

--- Project/Day-29 Password Manager/main.py
+++ Project/Day-29 Password Manager/main.py
@@ -1,4 +1,5 @@
 # Password Generator Project
+import json
 from random import choice, randint, shuffle
 from tkinter import *
 from tkinter import messagebox
@@ -45,27 +46,56 @@ def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
 
     # validate the details entered
-    if len(website) == 0 or len(email) == 0 or len(password) == 0:
+    if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="Please don't leave any fields empty!")
-
     else:
-        is_ok = messagebox.askokcancel(title=website,
-                                       message=f"These are the details entered: \nEmail: {email} "
-                                               f"\nPassword: {password} Is it ok to save?")
+        try:
+            with open("data.json", "r") as data_file:
+                # data_file.write(f"{website} | {email} | {password}\n")
+                # Reading old data
+                data = json.load(data_file)
+        except (FileNotFoundError, json.decoder.JSONDecodeError):
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            # Updating old data with new data
+            data.update(new_data)
 
-        if is_ok:
-            # 'with' closes the file after loop is complete
-            with open("data.txt", "a") as data_file:
-                data_file.write(f"{website} | {email} | {password}\n")
-                # clears the inputs
-                website_entry.delete(0, END)
-                password_entry.delete(0, END)
+            with open("data.json", "w") as data_file:
+                # Saving updated data
+                json.dump(data, data_file, indent=4)  # indent improves readability
+        finally:
+            # clears the inputs
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
+
+
+# ---------------------------- FIND PASSWORD ------------------------------- #
+def find_password():
+    website = website_entry.get()
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data File Found")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {email} \nPassword: {password}")
+        else:
+            messagebox.showinfo(title="Error", message=f"No details for {website} exists.")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
-
 window = Tk()
 window.title("Password Manager")
 window.config(padx=50, pady=50)
@@ -87,23 +117,26 @@ password_label = Label(text="Password:")
 password_label.grid(column=0, row=3)
 
 # Entries
-website_entry = Entry(width=35)
-website_entry.grid(column=1, row=1, columnspan=2)
+website_entry = Entry(width=21)
+website_entry.grid(column=1, row=1, sticky="ew")
 website_entry.focus()  # Places your cursor
 
 email_entry = Entry(width=35)
-email_entry.grid(column=1, row=2, columnspan=2)
+email_entry.grid(column=1, row=2, columnspan=2, sticky="ew")
 # Sets a default value in the beginning, in the end if used 'END' instead of 0
 email_entry.insert(0, "nilay@gmail.com")
 
 password_entry = Entry(width=21)
-password_entry.grid(column=1, row=3)
+password_entry.grid(column=1, row=3, sticky="ew")
 
 # Button
+search_button = Button(text="Search", width=13, command=find_password)
+search_button.grid(column=2, row=1, sticky="ew")
+
 generate_password_button = Button(text="Generate Password", command=generate_password)
-generate_password_button.grid(column=2, row=3)
+generate_password_button.grid(column=2, row=3, sticky="ew")
 
 add_button = Button(text="Add", width=36, command=save)
-add_button.grid(column=1, row=4, columnspan=2)
+add_button.grid(column=1, row=4, columnspan=2, sticky="ew")
 
 window.mainloop()
